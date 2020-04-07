d3bd215d54395098fb70d7add07df0bb888286e9
gavin murambadoro <developer.gavin@gmail.com>
2020-04-07 16:13:15 +0200

    Creating the vhost file and activating the site

--- vhost-activate.py
+++ vhost-activate.py
@@ -19,18 +19,24 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('domain', help="The name of the domain e.g. test.com. The suffix *.localhost will be appended "
                                    "automatically")
+
 parser.add_argument('--dir', required=False, help="The path to the document_root. "
                                                   "You can specify an absolute url or a path "
                     "relative to the Apache web_root folder {WEB_ROOT}".replace('{WEB_ROOT}', WEB_ROOT))
+
 parser.add_argument('--no-localhost', required=False, help="Specify this flag if you do not want *.localhost to "
                     "be appended automatically to the domain. However, you will need to add this domain to "
                     "/etc/hosts file yourself", action="store_true")
+
+parser.add_argument('--override', required=False, action='store_true',
+                    help='Override a vhost if the configuration file already exists.')
 
 args = parser.parse_args()
 
 domain = getattr(args, 'domain')
 dir_name = getattr(args, 'dir')
 no_localhost = getattr(args, 'no_localhost')
+override = getattr(args, 'override')
 
 if dir_name is None:
     dir_name = domain
@@ -74,15 +80,18 @@ with open(vhost_conf_src, 'w') as file:
     file.write(vhost_str)
 
 # create the destination path to store the config
-vhost_conf_dest = path.join(APACHE_SITES_FOLDER, vhost_conf_src)
+vhost_conf_dest = path.join(APACHE_SITES_FOLDER, path.basename(vhost_conf_src))
 
 if path.exists(vhost_conf_dest):
-    raise FileExistsError('The file `%s` already exists.' % vhost_conf_dest)
+    if override:
+        os.system('sudo rm %s' % vhost_conf_dest)
+    else:
+        raise Exception('The file `%s` already exists.' % vhost_conf_dest)
 
 shutil.copy(vhost_conf_src, vhost_conf_dest)
 
 # activate the site
-os.system('sudo systemctl a2ensite %s' % path.basename(vhost_conf_dest))
+os.system('sudo a2ensite %s' % path.basename(vhost_conf_dest))
 
 # restart apache 2
 os.system('sudo systemctl reload apache2')
